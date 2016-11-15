20429aee06e4055a858be1bd4c82d08cc0506929
caezar47 <iss47@ya.ru>
2016-11-15 16:33:00 +0300

    add pages

--- src/block/site/default/head/.document-ready.js
+++ src/block/site/default/head/.document-ready.js
@@ -1,18 +1,16 @@
 $('img').addClass('img-responsive');
 $('.text-block ul').addClass('ul-site');
-$('#ninja-slider ul').removeAttr('class');
-$('#thumbnail-slider ul').removeAttr('class');
 var url = window.location.pathname;
 $('.navbar-nav a[href="'+url+'"]').parent().addClass('active'); 
 $('.navbar-nav__razdel a[href="'+url+'"]').parent().addClass('active'); 
-$('.tabs-block a[href="'+url+'"]').parent().addClass('active'); 
+$('.navbar-nav-services a[href="'+url+'"]').parent().addClass('active'); 
 $('._ipc__slider-carousel .item').eq(0).addClass('active');
 $('._isb__carousel').carousel({
 	interval: 4000
 });
 
-$('.text-block table').addClass('table table-bordered');
-$('.text-block .table.table-bordered').parent().addClass('table-responsive');
+$('.text-block table').addClass('table table-striped');
+$('.text-block table.table-striped').wrap('<div class="table-responsive"></div>');
 $('.text-block img').parent().addClass('_tb__img'); 
 $('.fancybox').fancybox({
 	prevEffect : 'none',
@@ -51,4 +49,24 @@ $('._iabb__owl').owlCarousel({
 	autoplayTimeout:10000,
 	nav: true,
 	dots: false		
+});	
+$('._bipc__owl').owlCarousel({
+	margin: 0,
+	navText: [],
+	nav: true,
+	dots: false	,
+	responsive : {
+		0: {items:1},
+		750: {items:2},
+		1025: {
+			items: 3
+		}
+	}	
+});	
+$('._wipc__owl').owlCarousel({
+	margin: 0,
+	navText:[],
+	nav: true,
+	dots: false	,
+	items:1
 });	
