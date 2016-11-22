7a25575c398d190c33800b5eb39d39c265327c1a
caezar47 <iss47@ya.ru>
2016-11-22 15:13:49 +0300

    fix bags

--- src/_/concat.window-scroll.js
+++ src/_/concat.window-scroll.js
@@ -1,1 +1,2 @@
+$(function(){var a=$(document).scrollTop(),o=$(".go-to-top");o.hasClass("active")?a<200&&o.removeClass("active"):a>200&&o.addClass("active")});
 var topPos=$(document).scrollTop(),snb=$(".navbar-site.index");snb.hasClass("scroll-sidebar")?topPos<500&&snb.removeClass("scroll-sidebar"):topPos>500&&snb.addClass("scroll-sidebar"),snb.hasClass("opacity")?topPos<200&&snb.removeClass("opacity"):topPos>200&&snb.addClass("opacity"),snb.hasClass("fixed")?topPos<400&&snb.removeClass("fixed"):topPos>400&&snb.addClass("fixed");
